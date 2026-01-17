edcf0c3dbe542b9746df29bcb3f069b1aa1f6ae1
Serge Nicolas <30392207+serge-nicolas@users.noreply.github.com>
2026-01-17 15:41:02 +0100

    Update entrypoint.sh to modify NVM setup and git config
    Comment out NVM installation and configuration steps. Add git configuration for user details and restore git status.

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,29 +14,31 @@ NODE_VERSION_USER=$(sudo cat $(pwd)/package.json | jq -r '.engines.node')
 echo .
 echo installing node version ${NODE_VERSION_USER}
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+# export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 nvm list
 nvm install $NODE_VERSION_USER
 nvm use $NODE_VERSION_USER
 
-echo using $(node -v)
-
-
 # needed for EAS
 git config --global --add safe.directory /root/build
+git restore .
 git status
 
 # installing expo globally
-npm i -g expo-cli
+npm i -g expo-cli eas-cli
 
 echo #### installing
 yarn install
 
 echo #### review and upgrade dependencies
 npx expo install --check
+
+echo #### init git globally
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
 
 echo #### build
 # change for your package.json command to build
